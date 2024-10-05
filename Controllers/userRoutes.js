105f3269eb4785298d5837c55b4d02bcaba2a2b2
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2024-10-05 16:23:54 -0300

    Testando end-point com Router e ejs

---
+++ Controllers/userRoutes.js
@@ -0,0 +1,12 @@
+
+//DEPENDENCES
+const express = require('express');
+const router = express.Router();
+
+router.get('/homepage', (req, res) =>{
+    res.render('homepage')
+})
+
+
+module.exports = router
+
